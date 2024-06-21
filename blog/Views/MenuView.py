e8a1adf62881b98579a1ed9ef5b79f8fab56430c
MiguelBonilla-sys <mabonillat@academia.usbbog.edu.co>
2024-06-20 22:00:42 -0500

    chore: Actualizar vista de menú para mostrar grupos de integrantes en carrusel

--- blog/Views/MenuView.py
+++ blog/Views/MenuView.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from blog.Models.IntegrantesModel import Integrantes
+from itertools import cycle
 
 def menu(request):
-    return render(request, 'blog/menu.html', {'title': 'Menu'})
+    integrantes = list(Integrantes.objects.all())
+    integrantes_ciclicos = cycle(integrantes)
+    integrantes_groups = [list(next(integrantes_ciclicos) for _ in range(4)) for _ in range(0, len(integrantes), 4)]
+    return render(request, 'blog/menu.html', {'title': 'Menu', 'integrantes_groups': integrantes_groups})
